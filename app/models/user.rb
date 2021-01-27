73f8cb0ec44ca98bcdeefeca25b466cd7daa37ce
Chris Scott <cam022007@coderacademy.edu.au>
2021-01-27 05:06:49 -0800

    added associations between users and address

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_secure_password
   validates :password_digest, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
+
+  has_one :address
 end
