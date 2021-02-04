bfb28fddef3edbb65b7c28460afd7dafd97c92e8
Chris Scott <cam022007@coderacademy.edu.au>
2021-02-04 04:28:10 -0800

    added stripe controller

---
+++ app/controllers/charges_controller.rb
@@ -0,0 +1,38 @@
+class ChargesController < ApplicationController
+  
+  def create
+    # finds the correct booking and stores it in variable
+    booking = Booking.find(params[:id])
+
+    session = Stripe::Checkout::session.create({
+      payment_method_types: ['card'],
+      line_items: [{
+        price_data: {
+          unit_amount: listing.price * 100,
+          currency: 'aud',
+          product_data: {
+            name: 'Flex Physio',
+          },
+        },
+        quantity: 1,
+      }],
+      mode: 'payment',
+      success_url: "http://localhost:3000/api//bookings/current/:id",
+      cancel_url: "http://localhost:3000/api/charges/cancel",
+    })
+    render json: { id: session.id }
+  end
+
+  def webhook
+    payload = request.body.read
+    puts payload.inspect
+  end 
+
+  def success
+    @booking = Booking.find(params[:booking_id])
+  end 
+
+  def cancel
+  end 
+
+end
