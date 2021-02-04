4a1dca1aaa650c549d275540289aaee2d631a545
Chris Scott <cam022007@coderacademy.edu.au>
2021-02-03 21:21:02 -0800

    added stripe, and set up some of the related files and keys

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
   has_many :bookings
 
   # validations:
-  validates :first_name, length: { in: 2..20 }
-  validates :last_name, length: { in: 2..20 }
-  validates :password_digest, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true
-  validates :phone_number, presence: true, uniqueness: true
+  # validates :first_name, length: { in: 2..20 }
+  # validates :last_name, length: { in: 2..20 }
+  # validates :password_digest, presence: true, uniqueness: true
+  # validates :email, presence: true, uniqueness: true
+  # validates :phone_number, presence: true, uniqueness: true
 end
