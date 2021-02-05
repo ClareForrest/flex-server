b6fdd3416817bba84dbc1808edc2a93714b36380
Clare Forrest <clare.a.forrest@gmail.com>
2021-02-05 17:16:50 +1100

    set up tests yet to work, for remaining _spec.rb files

--- spec/requests/addresses_spec.rb
+++ spec/requests/addresses_spec.rb
@@ -1,31 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe "Addresses", type: :request do
-  describe "GET addresses#create" do
-    # before(:example) do
-    #   @first_address = FactoryBot.create(:address)
-    #   # @second_address = FactoryBot.create(:address)
-    #   get addresses_path
-    #   @json_response = JSON.parse(response.body)
-    # end
+  describe "POST addresses#create" do
+    context 'when the address is valid' do
+      before(:example) do
+        @address_params = FactoryBot.attributes_for(:address)
+        post addresses_path, params: { address: @address_params }, headers: authenticated_header
+      end
 
-    # it 'returns http success' do
-    #   expect(response).to have_http_status(:success)
-    # end
+      it 'returns http created' do
+        expect(response).to have_http_status(:created)
+      end
 
-    # it 'contains the correct number of entries' do
-    #   expect(@json_response['addresses'].count).to eq(2)
-    # end
+      it 'saves the Address to the database' do
+        expect(Address.last.street).to eq(@address_params[:street])
+      end
 
-    # it 'address contains expected attributes' do
-    #   expect(@json_response['addresses'].first).to include({
-    #     'id' => @first_address.id, 
-    #     'user' => @first_address.user_id,
-    #     'street' => @first_address.street,
-    #     'suburb' => @first_address.suburb,
-    #     'state' => @first_address.state,
-    #     'postcode' => @first_address.postcode,
-    #   })
-    # end
+      context 'when the Address is invalid' do
+        before(:example) do
+          @address_params = FactoryBot.attributes_for(:address, :invalid)
+          post addresses_path, params: { address: @address_params }, headers: authenticated_header
+          p @address_params
+          @json_response = JSON.parse(response.body)
+        end
+        
+        # it 'returns http unprocessable entity' do
+        #   expect(response).to have_http_status(:unprocessable_entity)
+        # end
+
+        # it 'returns the correct number of errors' do
+        #   expect(@json_response['errors'].count).to eq(1)
+        # end
+
+        # it 'errors contains the correct error message' do
+        #   expect(@json_response['errors'].first).to eq("Street can't be blank")
+        # end
+      end
+    end
   end
 end
