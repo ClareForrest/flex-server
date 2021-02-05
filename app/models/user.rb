51e1127e51aeabc81f55bbe5eed664ecee136e09
Chris Scott <cam022007@coderacademy.edu.au>
2021-02-04 22:11:28 -0800

    refactoring

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,9 @@ class User < ApplicationRecord
   has_many :bookings
 
   # validations:
-  validates :first_name, length: { in: 2..20 }
-  validates :last_name, length: { in: 2..20 }
-  validates :password_digest, presence: true
-  validates :email, presence: true
-  validates :phone_number, presence: true
+  # validates :first_name, length: { in: 2..20 }
+  # validates :last_name, length: { in: 2..20 }
+  # validates :password_digest, presence: true
+  # validates :email, presence: true
+  # validates :phone_number, presence: true
+end
