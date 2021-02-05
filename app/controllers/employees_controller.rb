4e354fbcf43f1744759d41a4c40c878f46bab311
Chris Scott <cam022007@coderacademy.edu.au>
2021-02-04 23:13:32 -0800

    refactoring authentication so employees can access their availability and other employeee's!

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,5 +1,5 @@
 class EmployeesController < ApplicationController
-  before_action :authenticate_user
+  before_action :authenticate_user, only: [:create, :destroy]
   # before_action :authorise_user
   before_action :set_employee, only: [:show, :destroy]
 
