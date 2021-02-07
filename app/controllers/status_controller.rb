c51495ea468f7b0027118e4c090296f892974d3d
Clare Forrest <clare.a.forrest@gmail.com>
2021-02-07 21:35:46 +1100

    added in render statuses

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -2,6 +2,6 @@ class StatusController < ApplicationController
   before_action :authenticate_user
 
   def index
-    render json: {user: current_user}
+    render json: {user: current_user}, status: :ok #added in status::ok 3:30 7/2/21
   end 
 end
