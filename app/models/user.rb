9a1818aae6cdd45a827f34ffcbe5eccc651ea0b5
Chris Scott <cam022007@coderacademy.edu.au>
2021-02-08 20:39:04 -0800

    validations commented back in

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
   has_many :bookings
 
   # validations:
-  # validates :first_name, presence: true, length: { in: 2..20 }
-  # validates :last_name, presence: true, length: { in: 2..20 }
-  # validates :password_digest, presence: true
-  # validates :email, presence: true
-  # validates :phone_number, presence: true
+  validates :first_name, presence: true, length: { in: 2..20 }
+  validates :last_name, presence: true, length: { in: 2..20 }
+  validates :password_digest, presence: true
+  validates :email, presence: true
+  validates :phone_number, presence: true
 end
